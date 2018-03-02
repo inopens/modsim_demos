7f7cc2f407e03655581f2d67b388dedde43a927e
DaanVanHauwermeiren <daan.van.hauwermeiren@gmail.com>
2018-03-02 16:35:48 +0100

    add file to generalise loading of libs into notebooks

---
+++ load_libs.py
@@ -0,0 +1,18 @@
+%matplotlib inline
+
+# Importeren van functionaliteiten
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+import numpy as np
+import scipy as sp
+import pandas as pd
+import math
+from IPython.display import Image
+from ipywidgets import interact, fixed, FloatSlider, IntSlider, ToggleButtons
+
+%load_ext autoreload
+%autoreload 1
+import sys
+import os
+sys.path.append("functions")
+%aimport general_functions
